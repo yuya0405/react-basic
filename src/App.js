8d7100eb5a4a5677b8915a594899b140b07e7d2b
Yuya Ohbayashi <yuya9445@gmail.com>
2021-06-08 22:41:54 +0900

    22. useReducer + useContext組み合わせ : 実装

--- src/App.js
+++ src/App.js
@@ -6,10 +6,28 @@ import ApiFetch from './components/ApiFetch';
 import AppContext from './contexts/AppContext';
 import B from './components/B';
 import BasicReducer from './components/BasicReducer';
+import CompB from './components/CompB';
+
+import {useReducer} from 'react'
+
+const initialState = 0
+const reducer = (currentState, action) => {
+    switch(action) {
+        case 'add_1':
+            return currentState + 1
+        case 'multiple_3':
+            return currentState * 3
+        case 'reset':
+            return initialState
+        default:
+            return currentState
+    }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    <AppContext.Provider value={'value from App.js'}>
+    <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +36,9 @@ function App() {
           {/* <TimerContainer /> */}
           {/* <ApiFetch />
           <B /> */}
-          <BasicReducer />
+          {/* <BasicReducer /> */}
+          Count {count}
+          <CompB />
 
         </header>
       </div>
