72165c3afac859deb470ece7c23e9033093c35ff
Yuya Ohbayashi <yuya9445@gmail.com>
2021-06-09 09:36:15 +0900

    [WIP]24. useCallback

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import B from './components/B';
 import BasicReducer from './components/BasicReducer';
 import CompB from './components/CompB';
 
-import {useReducer} from 'react'
+import {useReducer, useState} from 'react'
 import Memo from './components/Memo';
+import CountDisplay from './components/CountDisplay';
+import CountClick from './components/CountClick';
 
 const initialState = 0
 const reducer = (currentState, action) => {
@@ -27,6 +29,17 @@ const reducer = (currentState, action) => {
 
 function App() {
   const [count, dispatch] = useReducer(reducer, initialState)
+
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+  const AddCount1 = () => {
+      setCount1(prevCount1 => prevCount1 + 1)
+  }
+  const AddCount2 = () => {
+      setCount2(prevCount2 => prevCount2 + 1)
+  }
+
   return (
     <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
       <div className="App">
@@ -40,7 +53,11 @@ function App() {
           {/* <BasicReducer /> */}
           {/* Count {count}
           <CompB /> */}
-          <Memo />
+          {/* <Memo /> */}
+          <CountDisplay name="count1" count1={count1}/>
+          <CountClick handleClick={AddCount1}>AddCount1</CountClick>
+          <CountDisplay name="count2" count2={count2}/>
+          <CountClick handleClick={AddCount2}>AddCount2</CountClick>
 
         </header>
       </div>
